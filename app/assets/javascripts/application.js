cf78c107da53c2348b8298ae034573d53e353d1b
Mike Torres <apolloproductionsdj@gmail.com>
2020-08-09 08:46:52 +0000

    added bootrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery 3
+//= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
